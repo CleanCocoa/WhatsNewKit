bf12f942cf5838b9c1be64d6b23a27d6cb45a6d4
Christian Tietze <christian.tietze@gmail.com>
2018-06-27 08:16:08 +0200

    initial commit

---
+++ WhatsNewKit/WhatsNewKit.h
@@ -0,0 +1,19 @@
+//
+//  WhatsNewKit.h
+//  WhatsNewKit
+//
+//  Created by Christian Tietze on 27.06.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WhatsNewKit.
+FOUNDATION_EXPORT double WhatsNewKitVersionNumber;
+
+//! Project version string for WhatsNewKit.
+FOUNDATION_EXPORT const unsigned char WhatsNewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatsNewKit/PublicHeader.h>
+
+
