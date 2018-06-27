f7142ef16cfc98ceee44a29c0b734e89f774c76e
Christian Tietze <christian.tietze@gmail.com>
2018-06-27 08:20:57 +0200

    import WhatsNewKit in example app

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -1,21 +1,14 @@
 //  Copyright © 2018 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
 import Cocoa
+import WhatsNewKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
