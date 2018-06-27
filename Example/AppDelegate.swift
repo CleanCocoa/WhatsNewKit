d1f5afab312ba3a4f5c7a17d911257f357d83617
Christian Tietze <christian.tietze@gmail.com>
2018-06-27 08:17:34 +0200

    add Example app target

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Christian Tietze on 27.06.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
